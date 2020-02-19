13cc55f20029a47f0a33f722de593a18168f3be5
Danjb1 <danbryce1@gmail.com>
2020-02-19 13:03:35 +0000

    Write output to file

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import InputModel from './src/models/input.model';
+import SolutionModel from './src/models/solution.model';
 import { readFile } from './src/reader';
+import { writeFile } from './src/output-writer';
 
-const files = [
-  './inputs/a_example.in',
-  './inputs/b_small.in',
-  './inputs/c_medium.in',
-  './inputs/d_quite_big.in',
-  './inputs/e_also_big.in',
+const filenames = [
+  'a_example.in',
+  'b_small.in',
+  'c_medium.in',
+  'd_quite_big.in',
+  'e_also_big.in',
 ];
 
 const approaches = [
@@ -14,17 +16,14 @@ const approaches = [
 ];
 
 // Read each file
-files.forEach(f => {
+filenames.forEach(filename => {
 
-  const input: InputModel = readFile(f);
+  const input: InputModel = readFile(`./inputs/${filename}`);
 
   // Apply each approach
   approaches.forEach(approach => {
     const solution: SolutionModel = approach.apply(input);
-    writeFile(
-      output,
-      f.substring(f.lastIndexOf('/') + 1, f.lastIndexOf("."))
-    );
+    writeFile(solution, `./outputs/${filename}`);
   });
 
 });
