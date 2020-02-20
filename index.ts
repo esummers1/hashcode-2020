d12d91a47b2ce750e5472bf870a9ba807a637a77
Chris Bertrand <cjb_bjc@hotmail.com>
2020-02-20 19:17:06 +0000

    shortestFiltered

--- index.ts
+++ index.ts
@@ -1,30 +1,28 @@
 // Helpers
-import { readFile } from './src/reader';
-import { writeFile } from './src/output-writer';
+import { readFile } from "./src/reader";
+import { writeFile } from "./src/output-writer";
 
 // Models
-import InputModel from './src/models/input.model';
-import SolutionModel from './src/models/solution.model';
+import InputModel from "./src/models/input.model";
+import SolutionModel from "./src/models/solution.model";
 
 // Approaches
-import MostBooksApproach from './src/approaches/most-books';
+import MostBooksApproach from "./src/approaches/most-books";
+import ShortestSignupFirstApproach from "./src/approaches/shortest-signup-first";
 
 const filenames = [
-  'a_example.txt',
-  'b_read_on.txt',
-  'c_incunabula.txt',
-  'd_tough_choices.txt',
-  'e_so_many_books.txt',
-  'f_libraries_of_the_world.txt'
+  "a_example.txt",
+  "b_read_on.txt",
+  "c_incunabula.txt",
+  "d_tough_choices.txt",
+  "e_so_many_books.txt",
+  "f_libraries_of_the_world.txt"
 ];
 
-const approaches = [
-  new MostBooksApproach('most-books')
-];
+const approaches = [new ShortestSignupFirstApproach("shortest")];
 
 // Read each file
 filenames.forEach(filename => {
-
   const input: InputModel = readFile(`./inputs/${filename}`);
 
   // Apply each approach
@@ -32,5 +30,4 @@ filenames.forEach(filename => {
     const solution: SolutionModel = approach.apply(input);
     writeFile(solution, `./outputs/${solution.approachName}_${filename}`);
   });
-
 });
--- src/approaches/shortest-signup-first.ts
+++ src/approaches/shortest-signup-first.ts
@@ -1,13 +1,12 @@
-import Approach from './approach';
-import InputModel from '../models/input.model';
-import SolutionModel from '../models/solution.model';
-import BookModel from '../models/book.model';
-import { removeExcessiveSignupLibraries } from '../utils';
+import Approach from "./approach";
+import InputModel from "../models/input.model";
+import SolutionModel from "../models/solution.model";
+import BookModel from "../models/book.model";
+import { removeExcessiveSignupLibraries } from "../utils";
 
 export default class ShortestSignupFirstApproach extends Approach {
   public apply(input: InputModel): SolutionModel {
-
-    console.log('Applying approach: ' + this.name);
+    console.log("Applying approach: " + this.name);
 
     const solution: SolutionModel = {
       approachName: this.name,
@@ -15,18 +14,25 @@ export default class ShortestSignupFirstApproach extends Approach {
       books: new Map<number, BookModel[]>()
     };
 
-    // Remove libraries we can't use
-    const validLibraries = removeExcessiveSignupLibraries(input);
-
     // Sort libraries by sign up time in ascending order
-    const sortedLibraries = validLibraries.sort((a, b) => {
+    const sortedLibraries = input.libraries.sort((a, b) => {
       return a.signupLength - b.signupLength;
     });
 
+    const booksRead = new Set<BookModel>();
+
     // Find libraries
     sortedLibraries.forEach(lib => {
-      solution.libraries.push(lib);
-      solution.books.set(lib.id, lib.books);
+      const newBooks = lib.books.filter(book => {
+        return !booksRead.has(book);
+      });
+
+      if (newBooks.length > 0) {
+        solution.libraries.push(lib);
+        solution.books.set(lib.id, newBooks);
+        // Remember which books we have read!
+        lib.books.forEach(book => booksRead.add(book));
+      }
     });
 
     return solution;
