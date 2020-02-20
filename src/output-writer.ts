b4f4affb751c1d97eacf4acc5a3c783e8003c5c8
joaolpgomes <joao.lp.gomes@gmail.com>
2020-02-20 17:26:56 +0000

    fix build

--- src/output-writer.ts
+++ src/output-writer.ts
@@ -1,18 +1,19 @@
-import * as fs from 'fs';
-import SolutionModel from './models/solution.model';
+import * as fs from "fs";
+import SolutionModel from "./models/solution.model";
 
 export const writeFile = (solution: SolutionModel, filename: string): void => {
-
-  console.log('Writing solution for ', filename);
+  console.log("Writing solution for ", filename);
   let wstream = fs.createWriteStream(`${filename}.output`);
+
+  if (!solution.ids) return;
 
   // Write number of slices
   wstream.write(`${solution.ids.length} \n`);
 
   // Write each slice
   solution.ids.forEach((id: number) => {
-      wstream.write(`${id} `);
+    wstream.write(`${id} `);
   });
 
   wstream.end();
-}
+};
