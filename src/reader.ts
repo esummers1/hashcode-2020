3a59e54f9953838923c2d2e8b7c40ff9a6765f89
esummers1 <eddie.summers1@gmail.com>
2020-02-20 23:03:17 +0000

    Tidy reader

--- src/reader.ts
+++ src/reader.ts
@@ -4,30 +4,28 @@ import Book from './models/book.model';
 import Library from './models/library.model';
 
 export const readFile = (fileName: string): InputModel => {
+  const lines = fs.readFileSync(fileName, "utf-8").split("\n");
+  const days = Number(lines[0].split(" ")[2]);
+  const books: Book[] = readBooks(lines[1]);
+  const libraries = getLibraries(lines, books);
+  return { days, libraries };
+}
 
-  const input: InputModel = {};
-
-  // Read file by line
-  const data = fs.readFileSync(fileName, "utf-8").split("\n");
-
-  // Get global info
-  const firstLine = data[0].split(" ");
-  input.days = Number(firstLine[2]);
-
-  // Get book scores
-  const books: Book[] = data[1].split(" ").map((value, index) => {
+const readBooks = (line: string): Book[] => {
+  return line.split(" ").map((value, index) => {
     return {
       id: Number(index),
       score: Number(value)
     };
   });
+}
 
-  // Get our libraries
+const getLibraries = (lines: string[], books: Book[]): Library[] => {
   const libraries: Library[] = [];
-  for (let i = 2; i < data.length - 2; i += 2) {
+  for (let i = 2; i < lines.length - 2; i += 2) {
 
     // Read library line
-    const libraryLine = data[i].split(" ").map(i => Number(i));
+    const libraryLine = lines[i].split(" ").map(i => Number(i));
     const library: Library = {
       id: i / 2 - 1,
       signupLength: libraryLine[1],
@@ -36,12 +34,10 @@ export const readFile = (fileName: string): InputModel => {
     }
 
     // Read books line
-    const booksLine = data[i + 1].split(" ").map(i => Number(i));
+    const booksLine = lines[i + 1].split(" ").map(i => Number(i));
     booksLine.forEach(id => library.books.push(books[id]));
 
     libraries.push(library);
   }
-
-  input.libraries = libraries;
-  return input;
+  return libraries;
 }
