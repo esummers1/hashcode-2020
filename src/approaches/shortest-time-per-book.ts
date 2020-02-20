70fe7d0313fc8d178d7cd2e5e05931dc4fcc7a0d
Chris Bertrand <cjb_bjc@hotmail.com>
2020-02-20 20:10:59 +0000

    Shortests

--- src/approaches/shortest-time-per-book.ts
+++ src/approaches/shortest-time-per-book.ts
@@ -2,9 +2,8 @@ import Approach from "./approach";
 import InputModel from "../models/input.model";
 import SolutionModel from "../models/solution.model";
 import BookModel from "../models/book.model";
-import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
 
-export default class ShortestSignupFirstApproach extends Approach {
+export default class ShortestTimePerBook extends Approach {
   public apply(input: InputModel): SolutionModel {
     console.log("Applying approach: " + this.name);
 
@@ -16,12 +15,10 @@ export default class ShortestSignupFirstApproach extends Approach {
 
     // Sort libraries by sign up time in ascending order
     const sortedLibraries = input.libraries.sort((a, b) => {
-      return a.signupLength - b.signupLength;
+      return a.books.length / a.signupLength - b.books.length / b.signupLength;
     });
 
     const booksRead = new Set<BookModel>();
-
-    var elapsed = 0;
 
     // Find libraries
     sortedLibraries.forEach(lib => {
@@ -30,20 +27,14 @@ export default class ShortestSignupFirstApproach extends Approach {
       });
 
       if (newBooks.length > 0) {
-        elapsed += lib.signupLength;
-
-        var numOfBooksToTake = (input.days - elapsed) / lib.booksPerDay;
+        solution.libraries.push(lib);
 
         // Sort books - highest score first
         const sortedBooks = newBooks.sort((a, b) => b.score - a.score);
-        var takenBooks = sortedBooks.slice(0, numOfBooksToTake);
+        solution.books.set(lib.id, sortedBooks);
 
-        if (takenBooks.length > 0) {
-          solution.libraries.push(lib);
-          solution.books.set(lib.id, takenBooks);
-          // Remember which books we have read!
-          takenBooks.forEach(book => booksRead.add(book));
-        }
+        // Remember which books we have read!
+        lib.books.forEach(book => booksRead.add(book));
       }
     });
 
